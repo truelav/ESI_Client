9feee8589cc3c06732b641211cc544fbb549b726
truelav <madan.serghei@yahoo.com>
2024-03-31 19:01:07 -0700

    APISlice Separated the userApiSlice and productApiSlice from monolithic apiSlice

--- src/app/api/productApiSlice.ts
+++ src/app/api/productApiSlice.ts
@@ -1,32 +1,80 @@
 import { ProductSchema } from "../../entities/Product/model/types/productSchema";
+import { FormDataProps } from "../../shared/ui/Modals/ImportProducts/ImportProductsModal";
 import {
     ProductsAPIData,
     ProductsBrandedSchemaInterface,
+    SelectedProductIdsSchema,
 } from "../schemas/Product";
 import { apiSlice } from "./apiSlice";
 
 const productApiSlice = apiSlice.injectEndpoints({
     endpoints: (build) => ({
         getAllProducts: build.query<ProductsAPIData, void>({
-            query: () => `/products`,
             providesTags: ["Product"],
+            query: () => `/products`,
         }),
-
         getGroupedProducts: build.query<ProductsBrandedSchemaInterface[], void>(
             {
-                query: () => `/products/brandedProducts`,
                 providesTags: ["Product"],
+                query: () => `/products/brandedProducts`,
             }
         ),
-
         getSingleProduct: build.query<ProductSchema, string | undefined>({
+            providesTags: ["Product"],
             query: (id) => `/products/${id}`,
+        }),
+        addSingleProduct: build.mutation({
+            invalidatesTags: ["Product"],
+            query: (product) => ({
+                url: `/products`,
+                method: "POST",
+                body: product,
+                formData: true,
+            }),
+        }),
+        addMultipleProducts: build.mutation<unknown, FormDataProps>({
+            query: (products) => ({
+                url: `products/addMultiple`,
+                method: "POST",
+                body: products,
+                formData: true,
+            }),
+        }),
+        editSingleProduct: build.mutation<
+            ProductSchema,
+            Partial<ProductSchema> & Pick<ProductSchema, "_id">
+        >({
+            invalidatesTags: ["Product"],
+            query: (product) => ({
+                url: `/products/${product._id}`,
+                method: "PUT",
+                body: product,
+                formData: true,
+            }),
+        }),
+
+        deleteSingleProduct: build.mutation<
+            { success: boolean; id: string },
+            string
+        >({
+            invalidatesTags: ["Product"],
+            query: (id) => ({
+                url: `/products/${id}`,
+                method: "DELETE",
+            }),
+        }),
+        deleteMultipleProducts: build.mutation<
+            { success: boolean; productsIds: [] },
+            SelectedProductIdsSchema
+        >({
+            invalidatesTags: ["Product"],
+            query: (productIds) => ({
+                url: `/products`,
+                method: "DELETE",
+                body: productIds,
+            }),
         }),
     }),
 });
 
-export const {
-    useGetAllProductsQuery,
-    useGetSingleProductQuery,
-    useGetGroupedProductsQuery,
-} = productApiSlice;
+export const { useGetAllProductsQuery } = productApiSlice;
---
+++ src/app/api/userApiSlice.ts
@@ -0,0 +1,45 @@
+import { apiSlice } from "./apiSlice";
+import { UserSchemaInterface } from "../schemas/User";
+
+const userApiSlice = apiSlice.injectEndpoints({
+    endpoints: (build) => ({
+        getAllUsers: build.query<UserSchemaInterface, void>({
+            providesTags: ["User"],
+            query: () => `/auth/users`,
+        }),
+
+        getUserProfile: build.query<UserSchemaInterface, string>({
+            providesTags: ["User"],
+            query: (id) => `/auth/users/${id}`,
+        }),
+
+        addUser: build.mutation<UserSchemaInterface, UserSchemaInterface>({
+            invalidatesTags: ["User"],
+            query: (user) => ({
+                url: `/auth/register`,
+                method: "POST",
+                body: user,
+            }),
+        }),
+
+        editUser: build.mutation<UserSchemaInterface, UserSchemaInterface>({
+            invalidatesTags: ["User"],
+            query: (user) => ({
+                url: `/auth/users`,
+                method: "PUT",
+                body: user,
+            }),
+        }),
+
+        deleteUser: build.mutation<{ success: boolean; id: string }, string>({
+            invalidatesTags: ["User"],
+            query: (id) => ({
+                url: `/auth/users/${id}`,
+                method: "DELETE",
+                body: id,
+            }),
+        }),
+    }),
+});
+
+export { userApiSlice };
